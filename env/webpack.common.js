f9b4d8adc8afa66245a1e1ceeda02e8238bf5e84
Evgeny Korytov <korytoff@gmail.com>
2022-09-14 20:57:46 +0300

    Fix a webpack5 configuration bug

--- env/webpack.common.js
+++ env/webpack.common.js
@@ -6,7 +6,7 @@ module.exports = {
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '',
   },
   module: {
     rules: [
--- env/webpack.prod.js
+++ env/webpack.prod.js
@@ -1,15 +1,13 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
-// const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
-    minimize: true,
     minimizer: [
-      new OptimizeCSSAssetsPlugin({}),
       '...',
+      new CssMinimizerPlugin(),
     ],
   },
 });
